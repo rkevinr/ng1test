2ed2b6af5578e9c92f179d27bd36806c2af6e74d
rkevinr <rkevinrnb@gmail.com>
2017-03-04 13:57:43 -0600

    basic Angular app and controller

---
+++ scripts/myApp.js
@@ -0,0 +1,6 @@
+
+var myApp = angular.module('myApp', []);  // [] are DEPENDENCIES
+
+myApp.controller('myFirstController', function() {
+        console.log("first controller instantiated.")
+    });
