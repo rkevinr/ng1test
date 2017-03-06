8ef07d2116d347a8c54b4984fc2a330d9297ecfc
rkevinr <rkevinrnb@gmail.com>
2017-03-06 11:20:29 -0600

    basic two-subpane SPA but no NAV yet

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,6 +1,34 @@
 
-var myApp = angular.module('myApp', []);  // [] are DEPENDENCIES
+var myApp = angular.module('myApp', ['ngMessages', 'ngRoute']);
 
-myApp.controller('myFirstController', function() {
-        console.log("first controller instantiated.")
-    });
+myApp.config(function($routeProvider, $locationProvider) {
+
+    $locationProvider.hashPrefix('');
+    
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'sub_pages/first.html',
+        controller:  'myFirstController'
+    })
+
+    .when('/second', {
+        templateUrl: 'sub_pages/second.html',
+        controller:  'secondPaneController'
+    }) ;
+
+});
+
+function myRandomFunc(thisThing, thatThing, theOtherThing) {
+
+}
+
+myApp.controller('myFirstController', function($scope, $log) {
+        $log.info("first controller instantiated...scope is:")
+        $log.info($scope);
+        $log.info(angular.injector().annotate(myRandomFunc));
+});
+
+myApp.controller('secondPaneController', function($scope, $log) {
+        $log.info("second pane instantiated...scope is:")
+});
